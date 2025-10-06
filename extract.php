2ee16a4a271c59dafbe274fc87d076d4407f64ee
TimBroeni <tim.broenimann@stud.fhgr.ch>
2025-10-06 21:06:05 +0200

    php stuff + aufräumen

--- extract.php
+++ extract.php
@@ -1,7 +1,6 @@
 <?php
-
 function fetchServerData(){
-    $url = 'https://mcapi.us/server/status?ip=gommehd.net';
+    $url = 'https://mcapi.us/server/status?ip=play.manacube.com';
     // array erstellen mit allen 4 api's drin (unterschiedliche endungen)
 
 
@@ -11,6 +10,13 @@ function fetchServerData(){
 
     // Setzt Optionen
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Content-Type: application/json",
+    "Accept: application/json",
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) " .
+                   "AppleWebKit/537.36 (KHTML, like Gecko) " .
+                   "Chrome/118.0.5993.90 Safari/537.36"
+]);
 
     // Führt die cURL-Sitzung aus und erhält den Inhalt
     $response = curl_exec($ch);
@@ -18,10 +24,17 @@ function fetchServerData(){
     // Schließt die cURL-Sitzung
     curl_close($ch);
 
+//echo '<pre>';
+//print_r ($response);
+//echo '</pre>';
+
     // Dekodiert die JSON-Antwort und gibt Daten zurück
     return json_decode($response, true);
 }
-return fetchServerData();
+
+echo '<pre>';
+print_r(fetchServerData());
+echo '</pre>';
 
 
 
@@ -29,3 +42,4 @@ return fetchServerData();
 // https://mcapi.us/server/status?ip=mc.hypixel.net
 // https://mcapi.us/server/status?ip=mineplex.com
 // https://mcapi.us/server/status?ip=play.manacube.com
+// https://mcapi.us/server/status?ip=gommehd.net
